5b268e8f032d7fafe7fee879005b8dc784308e02
lswans <lucasswanson88@outlook.com>
2024-05-15 11:57:48 -0700

    connected backend server to ironarachne name generator api

--- src/NpcNamesApi.js
+++ src/NpcNamesApi.js
@@ -1,5 +1,6 @@
 import * as MUN from "@ironarachne/made-up-names";
 import React, { useState, useEffect } from 'react';
+import Button from '@mui/material/Button';
 export default function NpcNamesApi(props){
     let race = props.race;
     let gender = props.gender;
@@ -8,18 +9,28 @@ export default function NpcNamesApi(props){
     //let url = "https://names.ironarachne.com/race/"+race+"/"+gender+"/"+total;
     
     const [data, setData] = useState(null);
-
+    const [showNames, setShowNames] = useState(false);
     // Note: the empty deps array [] means
     // this useEffect will run once
     // similar to componentDidMount()
+    /*
     React.useEffect(() => {
         fetch("/api")
         .then(res => res.json())
         .then((data) => setData(data.message, data.text, data.status))
     })
+*/
+    async function getResponse(){
+        setShowNames(true);
+        fetch("/api")
+        .then(res => res.json())
+        .then((data) => setData(data.message, data.text, data.status))
+    }
+    console.log(data);
     return (
         <div>
-            <p>{data}</p>
+            <Button onClick={getResponse} variant={"outlined"}>Get NPC Names</Button>
+            {showNames && data}
         </div>
       );
     }
--- src/server/index.js
+++ src/server/index.js
@@ -1,20 +1,38 @@
+
 const express = require("express");
 const http = require('http');
 const https = require('https');
+const axios = require('axios');
+
+
+
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+//const response = await apiRequest();
 
-const sendRequest = async () => {
-    app.get("/api", (req, res) => {
-        //const names = fetch("https://names.ironarachne.com/race/elf/female/10")
-        res.json({ message: "Hello from Express backend!"});
-      });
-}
+let result = [];
 
+app.get("/api", (req, res) =>  {
+    //const names = fetch("https://names.ironarachne.com/race/elf/female/10")
+    apiRequest(res);
 
-sendRequest()
+     //console.log(response);
+     //res.json({ message: "hello!!!"});
+     //console.log(result);
+     //apiRequest();
+});
+async function apiRequest(res) {
+    let data = await axios.get('https://names.ironarachne.com/race/elf/female/10')
+    .then(response => {
+        console.log(response.data.names)
+        res.json({message: response.data.names})
+        })
+        //console.log(data);
+        //console.log(result);
+        
+    }  
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
