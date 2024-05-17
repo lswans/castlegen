c67a1ab506b72b16ed9acc540c75e10555133ede
lswans <lucasswanson88@outlook.com>
2024-05-16 17:25:54 -0700

    hid firebase api key in env file

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDLC41g3i9AtXFxcOblu1OxLk_fK7HsinU",
+  apiKey: process.env.FIREBASE_API_KEY,
   authDomain: "castlegen.firebaseapp.com",
   projectId: "castlegen",
   storageBucket: "castlegen.appspot.com",
